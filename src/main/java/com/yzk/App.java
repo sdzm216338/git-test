998541f46ce6fd622aaff427a9a9beea38e8479f
yzk216338 <yzk216338@163.com>
2023-07-06 22:44:29 +0800

    first commit

---
+++ src/main/java/com/yzk/App.java
@@ -0,0 +1,13 @@
+package com.yzk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "hello git test" );
+        System.out.println( "hello git test2" );
+    }
+}
