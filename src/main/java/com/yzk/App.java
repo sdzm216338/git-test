e3edc86aa5f3fd9009e6cb77387f30e7358e99f6
yzk216338 <yzk216338@163.com>
2023-07-06 23:05:06 +0800

    second commit

--- src/main/java/com/yzk/App.java
+++ src/main/java/com/yzk/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args ) {
         System.out.println( "hello git test" );
         System.out.println( "hello git test2" );
+        System.out.println( "hello git test3" );
     }
 }
