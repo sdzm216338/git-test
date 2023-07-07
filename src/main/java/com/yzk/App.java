593bc0ac7464aa5ac60104cce7da615585d8e0b4
sdzm216338 <138761819+sdzm216338@users.noreply.github.com>
2023-07-07 11:58:12 +0800

    gitee test commit

--- src/main/java/com/yzk/App.java
+++ src/main/java/com/yzk/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "hello git test3" );
         System.out.println( "push test" );
         System.out.println( "pull test" );
+        System.out.println( "gitee test !!!!" );
     }
 }
