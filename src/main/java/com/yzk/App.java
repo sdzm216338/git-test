8e7a0894409ff5508b40a41d1cf87036c762bf84
sdzm216338 <138761819+sdzm216338@users.noreply.github.com>
2023-07-07 11:18:13 +0800

    pull commit

--- src/main/java/com/yzk/App.java
+++ src/main/java/com/yzk/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "hello git test2" );
         System.out.println( "hello git test3" );
         System.out.println( "push test" );
+        System.out.println( "pull test" );
     }
 }
